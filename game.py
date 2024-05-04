3445724a3c0ed97418f9aeb891b994b451510975
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2024-05-04 11:46:08 +0100

    implemented multiple games, statistics, another heuristic AI

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import copy
+
 import numpy as np
 import argparse
 import time
@@ -129,8 +131,8 @@ class Game:
 
 
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, id):
+        self.id = id
 
     def move(self, game):
         pass
@@ -138,8 +140,7 @@ class Player:
 
 class HumanPlayer(Player):
     def __init__(self, id):
-        super().__init__()
-        self.id = id
+        super().__init__(id)
 
     def move(self, game):
         move = get_human_input(f"Player {self.id} select your column ")
@@ -149,11 +150,11 @@ class HumanPlayer(Player):
             move = get_human_input(f"Invalid Move - Player {p} select your column ")
         return move, r
 
-class RandomPlayer(Player):
 
+class RandomPlayer(Player):
+    """Completely random player"""
     def __init__(self, id):
-        super().__init__()
-        self.id = id
+        super().__init__(id)
 
     def move(self, game):
         move = np.random.randint(0, game.board.shape[1])
@@ -161,9 +162,50 @@ class RandomPlayer(Player):
             if r != -1:
                 break
             move = np.random.randint(0, game.board.shape[1])
-        time.sleep(1)
+        # time.sleep(1)
         return move, r
 
+
+class LookaheadPlayer(RandomPlayer):
+    """Random player that looks-ahead for a winning or blocking move"""
+    def __init__(self, id):
+        super().__init__(id)
+
+    def winning_move(self, game):
+        for c in range(game.board.shape[1]):
+            game_copy = copy.deepcopy(game)
+            r = game_copy.add_piece(c, self.id)
+            if r != -1:
+                if game_copy.check_all(r, c):
+                    return c
+        return -1
+
+    def blocking_move(self, game):
+        # search all possible moves for the other player to see if they can
+        # make one that will win. If we find one, we occupy that place on the
+        # board instead
+        for c in range(game.board.shape[1]):
+            game_copy = copy.deepcopy(game)
+            other_id = 2 if self.id == 1 else 1  # TODO: handle more than two players
+            r = game_copy.add_piece(c, other_id)
+            if r != -1:
+                if game_copy.check_all(r, c):
+                    return c
+        return -1
+
+    def move(self, game):
+        # check if we can win from the current state
+        move = self.winning_move(game)
+        if move != -1:
+            return move, game.add_piece(move, self.id)
+
+        # check if we can block the other player from winning
+        move = self.blocking_move(game)
+        if move != -1:
+            return move, game.add_piece(move, self.id)
+
+        # otherwise choose randomly
+        return super().move(game)
 
 
 def norm(arr):
@@ -201,35 +243,42 @@ def get_human_input(prompt):
         print("Invalid input")
 
 
-parser = argparse.ArgumentParser(description='Connect-4 Game')
-parser.add_argument('--nrows', type=int, default=6)
-parser.add_argument('--ncols', type=int, default=7)
-parser.add_argument('--nconnect', type=int, default=4)
-parser.add_argument('--player1', type=str, default="human")
-parser.add_argument('--player2', type=str, default="random")
-flags = parser.parse_args()
-
-
 def create_player(type, id):
     type = type.capitalize() + "Player"
     return globals()[type](id)
 
 
-game = Game(shape=(flags.nrows, flags.ncols), n_connect=flags.nconnect)
-players = [create_player(flags.player1, 1), create_player(flags.player2, 2)]
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Connect-4 Game')
+    parser.add_argument('--nrows', type=int, default=6)
+    parser.add_argument('--ncols', type=int, default=7)
+    parser.add_argument('--nconnect', type=int, default=4)
+    parser.add_argument('--player1', type=str, default="human")
+    parser.add_argument('--player2', type=str, default="random")
+    parser.add_argument('--num-games', type=int, default=1)
+    flags = parser.parse_args()
 
-p = 1
-print(game)
-while True:
-    move, r = players[p - 1].move(game)
+    game_stats = {'player1': 0, 'player2': 0, 'draw': 0}
+    for g in range(flags.num_games):
+        game = Game(shape=(flags.nrows, flags.ncols), n_connect=flags.nconnect)
+        players = [create_player(flags.player1, 1), create_player(flags.player2, 2)]
 
-    print(game)
-    if game.check_all(r, move):
-        print(f"Player {p} wins!")
-        break
+        p = 1
+        print(game)
+        while True:
+            move, r = players[p - 1].move(game)
 
-    if game.check_draw():
-        print("Draw has been reached")
-        break
+            print(game)
+            if game.check_all(r, move):
+                print(f"Player {p} wins!")
+                game_stats[f'player{p}'] += 1
+                break
 
-    p = 2 if p == 1 else 1
+            if game.check_draw():
+                print("Draw has been reached")
+                game_stats['draw'] += 1
+                break
+
+            p = 2 if p == 1 else 1
+
+    print(game_stats)
